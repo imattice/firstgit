3d3f1cc5d9aaf2e5f6a31935fb031f42d1d5a761
Ike Mattice & Rick Hills <student@epicodus.com>
2015-08-03 14:13:45 -0700

    Added set milage function

--- car.php
+++ car.php
@@ -5,39 +5,26 @@ class Car
     public $price;
     public $miles;
 
-    function __construct($model, $cost, $milage)
+    function __construct($model, $cost, $milage=0)
     {
       $this->make_model = $model;
       $this->price = $cost;
       $this->miles = $milage;
     }
+    function setMilage ($new_milage)
+    {
+      $float_milage = (float) $new_milage;
+      if ($float_milage !=0){
+        $formatted_milage = number_format($float_milage, 1);
+        $this->miles = $formatted_milage;
+      }
+    }
 }
 $porsche = new Car ("2014 Porche 911", 114991, 7864);
 $ford = new Car ("2011 Ford F450", 55995, 14241);
 $lexus = new Car ("2013 Lexus RX 350", 44700, 2000);
-$mercedes = new Car ("Mercedes Benz CLS550", 39900, 37979);
-
-
-/*$porsche = new Car();
-$porsche->make_model = "2014 Porsche 911";
-$porsche->price = 114991;
-$porsche->miles = 7864;
-
-$ford = new Car();
-$ford->make_model = "2011 Ford F450";
-$ford->price = 55995;
-$ford->miles = 14241;
-
-$lexus = new Car();
-$lexus->make_model = "2013 Lexus RX 350";
-$lexus->price = 44700;
-$lexus->miles = 20000;
-
-$mercedes = new Car();
-$mercedes->make_model = "Mercedes Benz CLS550";
-$mercedes->price = 39900;
-$mercedes->miles = 37979;
-*/
+$mercedes = new Car ("Mercedes Benz CLS550", 39900);
+$mercedes->setMilage("10");
 
 $cars = array($porsche, $ford, $lexus, $mercedes);
 ?>
